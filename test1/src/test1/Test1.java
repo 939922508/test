f6a7e5f6c5ebf0227b1f69f42ad1a12561e17631
86150 <86150@192.168.9.1>
2020-02-09 10:29:45 +0800

    1

--- test1/src/test1/Test1.java
+++ test1/src/test1/Test1.java
@@ -3,9 +3,6 @@ package test1;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println();
-		//111111111111111111111111
-		//6666666666666666666
-		//99999
-		//9999999999999999999
+	//22222222222222222222222222
 	}
 }
